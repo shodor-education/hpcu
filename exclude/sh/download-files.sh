33ebda102c88565a3c7531c28d7beeca43667dd4
Aaron Weeden <aweeden@shodor.org>
2024-01-15 15:19:31 -0500

    Move training resources to hpcu-training-materials repository.

--- exclude/sh/download-files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-while read line; do
-  dir=$(echo ${line} | cut -d'/' -f1);
-  mkdir -p ${dir};
-  curl http://hpcuniversity.org/media/TrainingMaterials/${line} -k --output ${line};
-done <${1};
--- exclude/sql/get-training-materials-files.sql
+++
@@ -1,6 +0,0 @@
-SELECT materialItem.`resourcePath`
-FROM trainingMaterials
-LEFT JOIN materialItem ON materialItem.`tmid` = trainingMaterials.`id`
-WHERE trainingMaterials.`isConfirmed` = 1
-AND materialItem.`resourcePath` IS NOT NULL
-ORDER BY materialItem.`displayOrder`
